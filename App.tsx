3e42ba31812d1c3c7ed4aec64a4ca71dcdb3a929
João Ferreiro <ferreiro@pinkroom.dev>
2022-10-28 20:23:02 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Web3Testament</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
